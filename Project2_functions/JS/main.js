ec6fc204af192d9bab710d1c3642540ef9d52f31
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-06-17 18:50:09 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() {  //Defining a function with a name
+    var sentence = "I am trying to learn"; //Defining variable as a sentence 
+    sentence += " all about JavaScript!"; //Concatenating the value of "sentence" with new string
+    document.getElementById("Concatenate") .innerHTML = sentence; //The result will be a modified HTML element
+}
